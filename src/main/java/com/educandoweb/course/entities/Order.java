cf86da653b67b7cfee6d8239aa364a1df4633e21
Nelio ALves <alvesjorge06@gmail.com>
2024-12-18 10:43:48 -0300

    SubTotal and Total

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -111,6 +111,14 @@ public class Order implements Serializable {
 		return set;
 	}
 	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
