5a22c32b0efdda3fbad5a9191cc1637a0fdbd5d2
Nelio ALves <alvesjorge06@gmail.com>
2024-12-18 13:37:39 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,6 +1,5 @@
 package com.educandoweb.course.services;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -10,26 +9,42 @@ import org.springframework.stereotype.Service;
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
 
+import jakarta.transaction.Transactional;
+
 @Service
 public class UserService {
 
-		@Autowired
-		private UserRepository repository;
-		
-		public List<User> findAll() {
-			return repository.findAll();
-		}
-		public User findById(Long id) {
-			Optional <User> obj = repository.findById(id);
-			return obj.get();
-		}
-		
-		public User insert(User obj) {
-			return repository.save(obj);
-		}
-		
-		public void delete(Long id) {
-			repository.deleteById(id);
-		}
-		
- }
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+
+	@Transactional
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
+}
